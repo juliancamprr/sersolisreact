604c6b84690de8579e84ee831601957b24d6e061
Djulian Campos <djuliancamposdc@gmail.com>
2023-05-22 12:38:56 -0300

    Navbar pronta

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,31 +1,75 @@
-
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
-import './Navbar.css'
+import "./Navbar.css";
 
 function Navbar() {
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
 
-
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
+  };
   return (
-
     <main>
       <div>
         <header>
           <div className="navbar">
             <div className="logo">
-              <Link to="/"><a href="/">Sersolis</a></Link>
+              <Link to="/">Sersolis</Link>
             </div>
             <ul className="links">
-             
-                <li><Link to="/">Início</Link></li>
-                <li><Link to="/sobre">Sobre</Link></li>
-                <li><Link to="/servicos">Serviços</Link></li>
+              <li>
+                <Link to="/">Início</Link>
+              </li>
+              <li>
+                <Link to="/sobre">Sobre</Link>
+              </li>
               
+              <li>
+                <Link to="/servicos">Serviços</Link>
+              </li>
             </ul>
-            <a href="https://discord.gg/5RBBmsW8Cx" target="_blank" rel="noreferrer" className="action_btn">Servidor</a>
-            <div className="toggle_btn">
-              
+            <a
+              href="https://discord.gg/5RBBmsW8Cx"
+              target="_blank"
+              rel="noreferrer"
+              className="action_btn"
+            >
+              Servidor
+            </a>
+            <div className="toggle_btn" onClick={toggleMobileMenu}>
+              <i
+                className={`fa-solid ${
+                  isMobileMenuOpen ? "fa-xmark" : "fa-bars"
+                }`}
+                style={{ color: "#ffffff" }}
+              ></i>
             </div>
+
+            {isMobileMenuOpen && (
+              <div
+                className={`dropdown_menu ${isMobileMenuOpen ? "open" : ""}`}
+              >
+                <ul>
+                  <li>
+                    <Link to="/">Início</Link>
+                  </li>
+                  <li>
+                    <Link to="/sobre">Sobre</Link>
+                  </li>
+                  <li>
+                    <Link to="/servicos">Serviços</Link>
+                  </li>
+                  <li>
+                    <Link className="text-gray-400 hover:text-gray-400 cursor-default">Equipe</Link>
+                    <p id="deactivate">Desativado</p>
+                  </li>
+                  <li className="servidor">
+                    <Link to="https://discord.gg/5RBBmsW8Cx">Servidor</Link>
+                  </li>
+                </ul>
+              </div>
+            )}
           </div>
         </header>
       </div>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,23 @@
-import Navbar from "../../Components/Navbar/Navbar";
+
 import "./Home.css";
 
 function Home() {
   return (
     <body>
-      <h1>Página principal</h1>
+         <main>
+      <div class="container">
+        <div class="illustration">
+          <img src="/images/sersolislogo.png" alt="" />
+        </div>
+        <div class="head-text text-white">
+          <h2>
+            Personalize seu <span>servidor profissional</span> de acordo com
+            suas necessidades, com nossos serviços adaptáveis
+          </h2>
+        </div>
+      </div>
+    </main>
+
     </body>
   );
 }
